c1dabacbc8530740df420af8ea4ca8af201570e8
AndersonKeller <andersonkellervendas@hotmail.com>
2025-05-23 21:52:14 -0300

    add controller

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -4,5 +4,6 @@ export const createPostController=async(req,res)=>{
     bodyValues.push(req.user.id)
     //
     //insert into post values(1,'teste','teste content',9);
-
+//create table post (id SERIAL primary key, name varchar(255), 
+// content varchar(255),user_id INT, CONSTRAINT fk_user foreign key(user_id) references usuarios(id) );
 }
