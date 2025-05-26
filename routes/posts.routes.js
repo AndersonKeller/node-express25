228258c832c7f04edcb1239cdd3cc597031f0526
AndersonKeller <andersonkellervendas@hotmail.com>
2025-05-26 20:01:43 -0300

    add: query

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -7,6 +7,11 @@ import { getUserByIdMiddleware } from "../middleware/getUserById.middleware.js"
 
 export const postRoutes = Router()
 
+// create table post_usuarios (id serial primary key, user_id int, post_id int,
+// 	constraint fk_user foreign key(user_id) references usuarios(id),
+// 	constraint fk_post foreign key(post_id) references post(id)
+// );
+
 postRoutes.post("",validateDataMiddleware(createPostSchema), validateTokenMiddleware,createPostController)
 postRoutes.get("",getAllPostsController)
 postRoutes.get("/:id",getUserByIdMiddleware, getPostByUserIdController)
