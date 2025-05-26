4d384c2698a2118e93715aed1e0fd2e62ecdb511
AndersonKeller <andersonkellervendas@hotmail.com>
2025-05-26 20:01:00 -0300

    add: posts controllers

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,7 +1,12 @@
 import {Router} from "express"
 import { validateTokenMiddleware } from "../middleware/validateToken.middleware.js"
-import { createPostController } from "../controllers/posts.controller.js"
+import { createPostController, getAllPostsController, getPostByUserIdController } from "../controllers/posts.controller.js"
+import { validateDataMiddleware } from "../middleware/validateData.middleware.js"
+import { createPostSchema } from "../schemas/posts.schemas.js"
+import { getUserByIdMiddleware } from "../middleware/getUserById.middleware.js"
 
 export const postRoutes = Router()
 
-postRoutes.post("",validateTokenMiddleware,createPostController)
+postRoutes.post("",validateDataMiddleware(createPostSchema), validateTokenMiddleware,createPostController)
+postRoutes.get("",getAllPostsController)
+postRoutes.get("/:id",getUserByIdMiddleware, getPostByUserIdController)
